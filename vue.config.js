fdfb1df7851e93d738a14dbac5153f2a40b62abb
mrleidesen <mrleidesen@outlook.com>
2021-05-13 12:04:00 +0800

    ci: github actions

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  productionSourceMap: false,
+  publicPath: isProd ? '/VPhone/' : '/',
 }
