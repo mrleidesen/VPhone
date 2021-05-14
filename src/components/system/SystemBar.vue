fd5800e392c918fd6900206e845a0b2ca455704c
mrleidesen <mrleidesen@outlook.com>
2021-05-14 09:28:18 +0800

    feat: mask icons

--- src/components/system/SystemBar.vue
+++ src/components/system/SystemBar.vue
@@ -7,7 +7,7 @@
     fixed
     class="system-bar"
   >
-    <span>{{ formatTime }}</span>
+    <span class="font-medium">{{ formatTime }}</span>
 
     <v-spacer></v-spacer>
 
@@ -23,7 +23,41 @@
       class="system-bar-mask"
       :class="{'system-bar-mask--dragging': isDragging, 'system-bar-mask--active': isDown}"
       :style="`transform: translateY(${offset}px)`"
-    ></div>
+    >
+      <div class="system-bar-wrapper">
+        <div class="system-bar-icons">
+          <v-container>
+            <div class="flex items-center">
+              <span class="text-3xl font-semibold text-white">{{ formatTime }}</span>
+            </div>
+          </v-container>
+          <v-container>
+            <v-row>
+              <v-col 
+                v-for="icon in icons"
+                :key="icon.id"
+                cols="3"
+              >
+                <div class="flex flex-col items-center justify-center">
+                  <div 
+                    class="w-10 h-10 rounded-full flex justify-center items-center overflow-hidden"
+                    :class="iconActive.includes(icon.id) ? 'bg-white' : 'bg-gray-500'"
+                    @click="onToggleIconAcitve(icon.id)"
+                  >
+                    <v-icon
+                      size="large"
+                      :color="iconActive.includes(icon.id) ? 'grey darken-4': 'white'" 
+                      class="mr-0"
+                    >{{ icon.icon }}</v-icon>
+                  </div>
+                  <div class="text-white text-xs mt-2 truncate">{{ icon.label }}</div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
+      </div>
+    </div>
   </v-system-bar>
 </template>
 
@@ -40,7 +74,50 @@ export default {
       offset: 0,
       isDragging: false,
       isDown: false,
-      lastEndOffset: 0
+      lastEndOffset: 0,
+      iconActive: [1, 2, 6],
+      icons: [
+        {
+          id: 1,
+          label: 'Wifi',
+          icon: 'mdi-wifi'
+        },
+        {
+          id: 2,
+          label: '移动数据',
+          icon: 'mdi-cable-data'
+        },
+        {
+          id: 3,
+          label: '蓝牙',
+          icon: 'mdi-bluetooth'
+        },
+        {
+          id: 4,
+          label: '静音',
+          icon: 'mdi-bell-cancel-outline'
+        },
+        {
+          id: 5,
+          label: '飞行模式',
+          icon: 'mdi-airplane'
+        },
+        {
+          id: 6,
+          label: '定位服务',
+          icon: 'mdi-map-marker'
+        },
+        {
+          id: 7,
+          label: '亮度',
+          icon: 'mdi-brightness-6'
+        },
+        {
+          id: 8,
+          label: '手电筒',
+          icon: 'mdi-flashlight'
+        },
+      ]
     }
   },
 
@@ -90,18 +167,29 @@ export default {
       this.isDragging = false
       const maxHeight = document.body.offsetHeight - 25
       const touchEndChangeOffset = e.touchstartY - e.touchendY
+      const autoChangeMaxOffset = 70
 
-      if (touchEndChangeOffset <= -80) {
+      if (touchEndChangeOffset <= -autoChangeMaxOffset) {
         this.offset = maxHeight
         this.lastEndOffset = maxHeight
         this.isDown = true
-      } else if (e.touchstartY - e.touchendY >= 80) {
+      } else if (e.touchstartY - e.touchendY >= autoChangeMaxOffset) {
         this.offset = 0
         this.lastEndOffset = 0
         this.isDown = false
       } else {
         this.offset = this.lastEndOffset
         this.isDown = this.lastEndOffset === maxHeight
+      }
+    },
+
+    onToggleIconAcitve(id) {
+      const { iconActive } = this
+      
+      if (iconActive.includes(id)) {
+        this.iconActive = iconActive.filter(v => v !== id)
+      } else {
+        this.iconActive = [...iconActive, id]
       }
     },
   },
@@ -121,26 +209,29 @@ export default {
     left: 0;
     width: 100%;
     height: 100vh;
+    box-sizing: border-box;
+    padding: 8px;
     opacity: 0;
     transform: translateY(0);
     transition: transform .3s ease-in-out, opacity .3s ease-in-out, background-color .3s ease-in-out;
     user-select: none;
 
-    &:active {
-      background-color: #000;
-      opacity: .2;
-    }
-
     &--dragging {
       opacity: .5;
-      background-color: #000;
+      background-color: rgba(0, 0, 0, 0.5);
       transition: transform 0s ease-in-out, opacity .3s ease-in-out, background-color .3s ease-in-out !important;
     }
 
     &--active {
-      opacity: .7;
-      background-color: #000;
+      opacity: 1;
+      background-color: rgba(0, 0, 0, 0.8);
     }
+  }
+
+  &-wrapper {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
   }
 }
 </style>
